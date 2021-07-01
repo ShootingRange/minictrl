01fff4b50fde7c745dac8da89bea136fc6091e83
Jener Rasmussen <jener@jener.dk>
2021-07-01 16:00:05 +0200

    Implement dataloaders for Team, Player and Match

--- src/web/graphql/dataloader.rs
+++ src/web/graphql/dataloader.rs
@@ -2,10 +2,28 @@ use std::collections::HashMap;
 
 use async_graphql::dataloader::Loader;
 use async_graphql::FieldError;
+use itertools::Itertools;
 use sqlx::types::Uuid;
 use sqlx::{Pool, Postgres};
+use IntoIterator;
 
-use crate::database::models::{Match, Team};
+use crate::database::models::{Match, Player, Team};
+
+/// Builds a list of UUIDs for use in a SQL query
+fn uuid_list(keys: &[Uuid]) -> String {
+    keys.iter()
+        .map(|key| format!("'{}'", key.to_string()))
+        .enumerate()
+        .fold(String::new(), |mut acc, (i, key)| {
+            if i == 0 {
+                key.to_string()
+            } else {
+                acc.push_str(", ");
+                acc.push_str(key.as_str());
+                acc
+            }
+        })
+}
 
 pub struct TeamLoader(Pool<Postgres>);
 
@@ -21,17 +39,73 @@ impl Loader<Uuid> for TeamLoader {
     type Error = FieldError;
 
     async fn load(&self, keys: &[Uuid]) -> Result<HashMap<Uuid, Self::Value>, Self::Error> {
-        let mut map = HashMap::with_capacity(keys.len());
+        let key_list = uuid_list(keys);
+        let query = format!("SELECT * FROM teams WHERE id IN ({})", key_list);
 
-        for key in keys {
-            /*map.insert(
-                *key,
-                todo!(),
-            );*/
-            todo!();
-        }
+        Ok(sqlx::query_as::<_, Team>(query.as_str())
+            .fetch_all(&self.0)
+            .await?
+            .drain(..)
+            .map(|row| (row.id, row))
+            .collect())
+    }
+}
 
-        Ok(map)
+pub struct PlayerLoader(Pool<Postgres>);
+
+impl PlayerLoader {
+    pub(in crate::web::graphql) fn new(postgres_pool: Pool<Postgres>) -> Self {
+        Self(postgres_pool)
+    }
+}
+
+#[async_trait]
+impl Loader<Uuid> for PlayerLoader {
+    type Value = Player;
+    type Error = FieldError;
+
+    async fn load(&self, keys: &[Uuid]) -> Result<HashMap<Uuid, Self::Value>, Self::Error> {
+        let key_list = uuid_list(keys);
+        let query = format!("SELECT * FROM teams WHERE id IN ({})", key_list);
+
+        Ok(sqlx::query_as::<_, Player>(query.as_str())
+            .fetch_all(&self.0)
+            .await?
+            .drain(..)
+            .map(|row| (row.id, row))
+            .collect())
+    }
+}
+
+pub struct PlayerTeamLoader(Pool<Postgres>);
+
+impl PlayerTeamLoader {
+    pub(in crate::web::graphql) fn new(postgres_pool: Pool<Postgres>) -> Self {
+        Self(postgres_pool)
+    }
+}
+
+#[async_trait]
+impl Loader<Uuid> for PlayerTeamLoader {
+    type Value = Vec<Player>;
+    type Error = FieldError;
+
+    async fn load(&self, keys: &[Uuid]) -> Result<HashMap<Uuid, Self::Value>, Self::Error> {
+        let key_list = uuid_list(keys);
+        let query = format!("SELECT * FROM players WHERE team_id IN ({})", key_list);
+
+        let mut players = sqlx::query_as::<_, Player>(query.as_str())
+            .fetch_all(&self.0)
+            .await?;
+        players.sort_by_key(|player| player.team_id);
+        let team_players: HashMap<Uuid, Self::Value> = players
+            .iter()
+            .group_by(|player| player.team_id)
+            .into_iter()
+            .map(|(team_id, group)| (team_id, group.cloned().collect::<Vec<Player>>()))
+            .collect();
+
+        Ok(team_players)
     }
 }
 
@@ -49,17 +123,15 @@ impl Loader<Uuid> for MatchLoader {
     type Error = FieldError;
 
     async fn load(&self, keys: &[Uuid]) -> Result<HashMap<Uuid, Self::Value>, Self::Error> {
-        let mut map = HashMap::with_capacity(keys.len());
+        let key_list = uuid_list(keys);
+        let query = format!("SELECT * FROM teams WHERE id IN ({})", key_list);
 
-        for key in keys {
-            /*map.insert(
-                *key,
-                todo!(),
-            );*/
-            todo!();
-        }
-
-        Ok(map)
+        Ok(sqlx::query_as::<_, Match>(query.as_str())
+            .fetch_all(&self.0)
+            .await?
+            .drain(..)
+            .map(|row| (row.id, row))
+            .collect())
     }
 }
 
